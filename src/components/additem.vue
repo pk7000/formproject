1e26a6e5035f85db27c850f9a01b2cbde8431095
pk7000 <68525782+pk7000@users.noreply.github.com>
2020-10-10 20:52:36 +0700

    first commit

---
+++ src/components/additem.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <p>เพศ</p>
+      <v-row >
+        <v-checkbox
+        v-model="sex"
+        value="ชาย"
+        label="ชาย"
+        required
+      ></v-checkbox>
+      <v-checkbox
+        v-model="sex"
+        value="หญิง"
+        label="หญิง"
+        required
+      ></v-checkbox></v-row>
+      <p>อายุ</p>
+      <v-row>
+        <v-checkbox
+        v-model="age"
+        value="teen"
+        label="น้อยกว่า 20 ปี"
+        required
+      ></v-checkbox>
+      <v-checkbox
+        v-model="age"
+        value="working"
+        label="20-40"
+        required
+      ></v-checkbox>
+      <v-checkbox
+        v-model="age"
+        value="midle"
+        label="41-60 ปี"
+        required
+      ></v-checkbox>
+      <v-checkbox
+        v-model="age"
+        value="aged"
+        label="61 ปีขึ้นไป"
+        required
+      ></v-checkbox></v-row>
+      <v-col>
+        <p>พนักงานมีความเป็นกันเอง</p>
+        <v-checkbox v-model="s1" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s1" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s1" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s1" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s1" value="0" label="แย่มาก" required></v-checkbox>
+      </v-col>
+      <v-col>
+        <p>พนักงานทำงานอย่างมีประสิทธิภาพ</p>
+        <v-checkbox v-model="s2" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s2" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s2" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s2" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s2" value="0" label="แย่มาก" required></v-checkbox>
+        </v-col>
+      <v-col>
+        <p>ความสะอาดของสถานที่</p>
+        <v-checkbox v-model="s3" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s3" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s3" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s3" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s3" value="0" label="แย่มาก" required></v-checkbox>
+        </v-col>
+      <v-col>
+        <p>สิ่งอำนวยความสะดวก</p>
+        <v-checkbox v-model="s4" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s4" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s4" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s4" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s4" value="0" label="แย่มาก" required></v-checkbox>
+        </v-col>
+      <v-col>
+        <p>ความปลอดภัย</p>
+        <v-checkbox v-model="s5" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s5" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s5" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s5" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s5" value="0" label="แย่มาก" required></v-checkbox>
+        </v-col>
+      <v-col>
+        <p>มีการแจ้งข่าวสารที่เป็นประโยชน์</p>
+        <v-checkbox v-model="s6" value="4" label="ดีมาก" required></v-checkbox>
+        <v-checkbox v-model="s6" value="3" label="ดี" required></v-checkbox>
+        <v-checkbox v-model="s6" value="2" label="พอใช้" required></v-checkbox>
+        <v-checkbox v-model="s6" value="1" label="แย่" required></v-checkbox>
+        <v-checkbox v-model="s6" value="0" label="แย่มาก" required></v-checkbox>
+        </v-col>
+      <v-col>
+        <p>ข้อเสนอแนะเพิ่มเติม</p>
+        <v-text-field></v-text-field>
+        </v-col>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Submit
+      </v-btn>
+      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+      <v-btn class="mr-4" @click="page">See all</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    sex: "",
+    age:"",
+    s1: 0,
+    s2: 0,
+    s3: 0,
+    s4: 0,
+    s5: 0,
+    s6: 0,
+    score: 0,
+  }),
+  methods: {
+    validate() {
+        let payload = {
+          score: this.s1/1+this.s2/1+this.s3/1+this.s4/1+this.s5/1+this.s6/1,
+          sex: this.sex,
+          age: this.age
+        };
+        this.$store.dispatch("setItem", payload);     
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    page() {
+      this.$store.state.page = 2;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/datatable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-row no-gutters>
+      <v-col md="3"><v-card class="pa-2 grey darken-3" outlined> id</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2 grey darken-3" outlined> sex</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2 grey darken-3" outlined> age</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2 grey darken-3" outlined> score</v-card></v-col>
+    </v-row>
+    <v-row v-for="(items, index) in $store.getters.items" :key="index"  no-gutters>
+      <v-col md="3"><v-card class="pa-2" outlined> {{ index + 1 }}</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2" outlined> {{ items.sex }}</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2" outlined> {{ items.age }}</v-card></v-col>
+      <v-col md="3"><v-card class="pa-2" outlined> {{ items.score }}</v-card></v-col>
+      <td></td>
+    </v-row>
+    <v-btn class="mr-4" @click="page">กรอกแบบสอบถามอีกครั้ง</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    page() {
+      this.$store.state.page = 1;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
